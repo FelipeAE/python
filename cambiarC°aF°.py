002b856fbfb635a7e74a72b7614300e905bb14f0
Felipe Alvarez <32912012+FelipeAE@users.noreply.github.com>
2022-05-25 17:02:57 -0400

    Add files via upload

---
+++ cambiarC°aF°.py
@@ -0,0 +1,4 @@
+# Escribir un programa que solicite al usuario el ingreso de una temperatura en escala Fahrenheit (debe permitir decimales) y le muestre el equivalente en grados Celsius. 
+temp=float(input("Ingrese temperatura en F°: "))
+Celsius= (5/9) * (temp-32)
+print("La temperatura en celsius es: "+str(Celsius)) 
